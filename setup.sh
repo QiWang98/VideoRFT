e72933b6253af154a1ac44ab4fa9c2be406f9afb
Qi Wang <qi_wang_@outlook.com>
2025-10-26 15:26:42 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,6 @@ pip install flash-attn --no-build-isolation
 pip install vllm==0.7.3
 
 # fix transformers version
+pip install transformers==4.51.3
+# The following is the transformers version of r1-v codebase
 # pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
